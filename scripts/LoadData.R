475dc3de25d91503a30eef7434a9b5aa842588b1
“potatoman” <jan-ake.husmann@uib.no>
2023-09-12 14:34:53 +0200

    Script now merges both original dataframes

--- scripts/LoadData.R
+++ scripts/LoadData.R
@@ -2,4 +2,7 @@ library(tidyverse)
 library(here)
 here()
 data1 <- read_delim(here("data_original", "exam_data.txt"))
+data1 <- distinct(data1) # removes duplicated columns, always check
 data2 <- read_delim(here("data_original", "exam_data_join.txt"))
+data2 <- distinct(data2)
+merged_data <- merge.data.frame(data1, data2)
