99dd036e164c40ffa7b660f7d09d9cc70bbed186
“potatoman” <jan-ake.husmann@uib.no>
2023-09-12 15:00:06 +0200

    Changes Column head

--- scripts/LoadData.R
+++ scripts/LoadData.R
@@ -6,3 +6,6 @@ data1 <- distinct(data1) # removes duplicated columns, always check
 data2 <- read_delim(here("data_original", "exam_data_join.txt"))
 data2 <- distinct(data2)
 merged_data <- merge.data.frame(data1, data2)
+merged_data2 <- merged_data %>%
+  rename(gender = '1gender',
+         BMI = `BMI kg/m2`) # renames column head
