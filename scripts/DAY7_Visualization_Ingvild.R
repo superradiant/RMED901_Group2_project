e361ff20841fb3830797da0f672ce2c7c6543243
ingvildbrus <ingvildsbruserud@gmail.com>
2023-09-14 15:04:02 +0200

    Ingvildscript has assignments from Day6 Visualizations are from Day 7

---
+++ scripts/DAY7_Visualization_Ingvild.R
@@ -0,0 +1,91 @@
+
+###4. Day 7: Create plots that would help answer these questions:
+
+- Are there any correlated measurements?
+#  - Does the age distribution depend on `treat`?
+#  - Does the age distribution of the patients depend on their sex (`gender`)?
+#  - Does the preoperative pain change with age of the patients?
+#  - Do BMI and age have a linear relationship?
+  
+  
+  #Does the age distribution depend on `treat`? the treat is bmi
+  
+  
+# age and amount of smoking
+  #Making a binary variable whether the patient smokes or not. HOWEVER, all patients are smokers
+merged_data2<- merged_data2 %>% 
+  mutate(smokingYN= ifelse(smoking == 0, "no smoking","smoking" ))
+smokingYN
+
+plot_smoking<-ggplot(merged_data2,
+                 aes(
+                   x=smokingYN,
+                   y= age)) +
+  geom_jitter(aes(col=smokingYN))
+plot_smoking
+
+#easy plot
+plot_smoking<-ggplot(merged_data2,
+                     aes(
+                       x=smoking,
+                       y= age)) +
+  geom_jitter(aes(col=smoking))
+plot_smoking
+
+
+#a histogram would probably show the data and/or trend better divided by age
+
+plot_smoking2<-ggplot(merged_data2, aes(x=smoking, y=age)) +
+                        geom_col(aes(fill = gender), position=position_dodge())
+plot_smoking2
+
+
+#plot age distribution and gender in a "jitter plot"
+plot_gender_age<-ggplot(merged_data2,
+                     aes(
+                       x=gender,
+                       y= age)) +
+  geom_jitter(aes(col=gender))
+plot_gender_age
+
+#Boxplot with ages
+boxplot_gender_age2<-ggplot(merged_data2,
+                        aes(
+                          x=gender,
+                          y= age)) +
+  geom_boxplot(aes(col=gender))
+boxplot_gender_age2
+
+#  - Does the preoperative pain change with age of the patients?
+preoperative_pain_plot<-ggplot(merged_data2,
+                               aes(
+                                 x=pain,
+                                 y= age)) +
+  geom_jitter(aes(col=pain))
+preoperative_pain_plot
+
+
+#preoperative pain and age histogram grouped my gender #putting pain as factor to avoid
+plot_preoperativepain<- ggplot(merged_data2, 
+                               aes(x=as.factor(pain), y=age)) + 
+  geom_col(aes(fill = age), position=position_dodge())+
+  facet_wrap(facets = vars(gender))+
+  xlab("Pain")+
+  ylab("Age")+
+  labs(title = "Preoperative pain",
+       subtitle = "Grouped by gender")
+plot_preoperativepain
+
+  
+#  - Do BMI and age have a linear relationship? This makes a scatterplot over these two variables
+scatterplotBMI_AGE<-ggplot(merged_data2,
+                           aes(x=age,
+                               y=bmi)) + 
+  geom_point()
+scatterplotBMI_AGE
+
+
+
+merged_data2 %>% glimpse()
+merged_data2 %>% count(smoking)
+
--- scripts/Ingvildscript.R
+++ scripts/Ingvildscript.R
@@ -1,9 +1,11 @@
 library(tidyverse)
 library(here)
 here()
-library(tidyverse)
 library(skimr)
-library(here)
+library(patchwork)
+library(devtools)
+library(ggplot2)
+library(usethis)
 
 
 ## a column cutting BMI into quartiles (4 equal parts); HINT: cut() function
@@ -31,4 +33,52 @@ merged_data2<- merged_data2 %>%
   mutate(bmi_4groups= cut(bmi, 4, labels = c("1st quartile", "2nd quartile", "3rd quartile", "4th quartile"))) %>% 
   select(patient_id, bmi, age, smoking, gender, everything()) %>% 
   arrange(patient_id)
+
+
+#Stratify your data by a categorical column and report min, max, mean and sd of a numeric column for a defined set of observations - use pipe!
+### We have used age as continuous variable
+
+###gender and age (min, max, mean and sd)
+merged_data2 %>% 
+  group_by(gender) %>% #stratified by gender
+  summarise(max(age, na.rm = T), min(age, na.rm = T), mean(age, na.rm = T), sd(age, na.rm=T))
+
+#create a variable telling if BMI is above BMI25
+merged_data2<-merged_data2 %>% 
+  mutate(BMI_above25= if_else(bmi>25, "BMI above 25", "BMI under 25"))
+View(merged_data2)
+
+#  Only for persons with BMI <25
+merged_data2 %>% 
+  filter(BMI_above25=="BMI under 25") %>% 
+  summarise(max(age, na.rm = T), 
+            min(age, na.rm = T), 
+            mean(age, na.rm = T), 
+            sd(age, na.rm=T))
+
+
+#Only females,  min, max, mean and sd for AGE
+merged_data2 %>% 
+  filter(gender== "Female") %>% #only women
+  summarise(max(age, na.rm = T), min(age, na.rm = T), mean(age, na.rm = T), sd(age, na.rm=T))
+
+#- Only for persons older than 50 years of age
+# age in patients above 50 years
+merged_data2 %>% 
+  filter(age> 50) %>% 
+  summarise(max(age, na.rm = T), 
+            min(age, na.rm = T), 
+            mean(age, na.rm = T), 
+            sd(age, na.rm=T)) 
+
+
+#- Only for persons who had experienced coughing at extubation
+merged_data2 %>% 
+  filter(extubation_cough>0) %>% #patient had cough 
+  summarise(max(age, na.rm = T), 
+            min(age, na.rm = T), 
+            mean(age, na.rm = T), 
+            sd(age, na.rm=T)) 
+
+
 
