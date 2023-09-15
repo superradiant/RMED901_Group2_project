67145904a612aba51a8d7d6700b070e1a20ac3b6
“potatoman” <jan-ake.husmann@uib.no>
2023-09-15 11:03:04 +0200

    Narrowed ingvilds script down a little, since its overlapping with LoadData script. Generated Group_2.Rmd RMarkdown document we can work on it together

--- scripts/Ingvildscript.R
+++ scripts/Ingvildscript.R
@@ -2,43 +2,16 @@ library(tidyverse)
 library(here)
 here()
 library(skimr)
+install.packages("patchwork")
 library(patchwork)
 library(devtools)
 library(ggplot2)
 library(usethis)
 
-
-## a column cutting BMI into quartiles (4 equal parts); HINT: cut() function
-merged_data2<- merged_data2 %>%
-  mutate(bmi_4groups= cut(bmi, 4, labels = c("1st quartile", "2nd quartile", "3rd quartile", "4th quartile")))
-
-#- a column coding gender to "Male" and "Female" instead of "0"/"1"
-merged_data2 <-
-  merged_data2 %>% 
-  mutate(gender = if_else(gender == "F", "Female", "Male"))
-
-View(merged_data2)
-
-#- Set the order of columns as: `patient_id, BMI, age, smoking, gender` and #other columns
-merged_data2<- merged_data2 %>% 
-  select(patient_id, bmi, age, smoking, gender, everything()) 
-
-#- Arrange patient_id column of your dataset in order of increasing number or #alphabetically.
-merged_data2<- merged_data%>%
-  arrange(patient_id)
-
-#- Connect above steps with pipe Ingvilds steps
-merged_data2<- merged_data2 %>% 
-  mutate(gender = if_else(gender == "F", "Female", "Male")) %>% 
-  mutate(bmi_4groups= cut(bmi, 4, labels = c("1st quartile", "2nd quartile", "3rd quartile", "4th quartile"))) %>% 
-  select(patient_id, bmi, age, smoking, gender, everything()) %>% 
-  arrange(patient_id)
-
-
 #Stratify your data by a categorical column and report min, max, mean and sd of a numeric column for a defined set of observations - use pipe!
-### We have used age as continuous variable
+#We have used age as continuous variable
 
-###gender and age (min, max, mean and sd)
+#gender and age (min, max, mean and sd)
 merged_data2 %>% 
   group_by(gender) %>% #stratified by gender
   summarise(max(age, na.rm = T), min(age, na.rm = T), mean(age, na.rm = T), sd(age, na.rm=T))
